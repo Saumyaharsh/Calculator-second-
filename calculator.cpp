0de0b92ffc0e55acd7c48d239331a37f05d82509
Saumya Harsh <135820722+Saumyaharsh@users.noreply.github.com>
2024-02-22 00:41:26 +0530

    calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -129,29 +129,5 @@ int main (){
     }
 
     }
-
-     /*
-     sorting ascending order
-     bubble sort
-     4 5 8 9 6
-     int arr[n]
-        for(int i = 0;i<n;i++){
-        for(int j = 0 ;j<n-1 - i;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
-            }
-        }
-    }
-    cout arr
-
-     */
-
-      
-       
-      
-        
-    
-
-
     return 0;
 }
